25ba3b4943739bd14f177a4c6c44cfdc6790b11f
ZR <zhongrong123@gmail.com>
2023-11-02 23:35:52 +0800

    added homepage and styles for homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,62 @@
-import React from 'react';
+import { useEffect } from 'react';
+import './App.css';
+import searchIcon from './search.svg';
+
+const API_URL = 'http://www.omdbapi.com?apikey=8cd2ac81';
+
+const dummyMovie = {
+  "Title": "Miles Morales Ultimate Spiderman",
+  "Year": "2021",
+  "imdbID": "tt14311386",
+  "Type": "movie",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BNmMzODkwNDktMTkyMy00MmU5LWE4MGMtYzIzZjdjNmJiZDRiXkEyXkFqcGdeQXVyNDU1NDQ0NzE@._V1_SX300.jpg"
+}
 
 const App = () => {
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  }
+
+  useEffect(() => {
+    searchMovies('Spiderman');
+  }, []);
+
   return (
-    <h1>App</h1>
+    <div className="app">
+      <h1>MovieDB</h1>
+
+      <div className="search">
+        <input 
+          placeholder="Search for movies"
+          value="Superman"
+          onChange={() => {}}
+        />
+        <img 
+          src={searchIcon}
+          alt="Search Icon"
+          onClick={() => {}}
+        />
+      </div>
+
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p>{dummyMovie.Year}</p>
+          </div>
+
+          <div>
+            <img 
+              src={dummyMovie.Poster}
+              alt={dummyMovie.Title}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
